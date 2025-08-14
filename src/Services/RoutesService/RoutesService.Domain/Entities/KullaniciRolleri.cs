430f51371dc3eeccb66c0042f4fe142b6a02d0a2
ilknurkosar <ilknurnazlikosar@gmail.com>
2025-08-14 11:58:34 +0300

    Fixed Role and Permission entities

---
+++ src/Services/RoutesService/RoutesService.Domain/Entities/KullaniciRolleri.cs
@@ -0,0 +1,11 @@
+﻿namespace RoutesService.Domain.Entities
+{
+    public class KullaniciRolleri
+    {
+        public int KullaniciId { get; set; }
+        public Kullanici? Kullanici { get; set; }
+
+        public int RolId { get; set; }
+        public Rol? Rol { get; set; }
+    }
+}
